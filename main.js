1045bd7c14773befb529e33c1adf5f8eb9fe5e12
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2021-02-26 01:04:17 +0100

    conditional rendering & more attr list

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ var product = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
-        image: './img/vmSocks.jpg'
+        image: './img/vmSocks.jpg',
+        inventory: '20',
+        details: ["80% contton", "20% polyyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: [" Large", "Medium", "Small"]
     }
 })
